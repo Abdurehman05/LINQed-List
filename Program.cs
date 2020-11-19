56abf876a53013b09040cd031ff9d60d3eda6b96
Abdu <abdukr5@gmail.com.com>
2020-11-19 13:58:07 -0600

    fruits filtered by L

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits where fruit.StartsWith("L") select fruit;
+
+            foreach (string fruit in LFruits) Console.WriteLine(fruit);
+        }
+    }
+}
