2260df8433c545aebe28985b741a62c571c88c23
Abdu <abdukr5@gmail.com.com>
2020-11-20 10:42:42 -0600

    numbers sorted in ascending order

--- Program.cs
+++ Program.cs
@@ -9,24 +9,24 @@ namespace linq
         {
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
-            IEnumerable<string> LFruits = from fruit in fruits where fruit.StartsWith("L") select fruit;
+            IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L"));
 
             foreach (string fruit in LFruits)
             {
                 Console.WriteLine(fruit);
             }
 
-            List<int> numbers = new List<int>()
-{
-    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-};
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
 
-            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
+            // IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
 
-            foreach (int number in fourSixMultiples)
-            {
-                Console.WriteLine(number);
-            }
+            // foreach (int number in fourSixMultiples)
+            // {
+            //     Console.WriteLine(number);
+            // }
 
 
 
@@ -46,6 +46,22 @@ namespace linq
             {
                 Console.WriteLine(name);
             }
+
+            // Build a collection of these numbers sorted in ascending order
+
+            List<int> numbers = new List<int>()
+                {
+                     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+                };
+
+
+            List<int> ascend = numbers.OrderBy(num => num).ToList();
+
+            foreach (int num in ascend)
+            {
+                Console.WriteLine(num);
+            }
+
         }
     }
 }
